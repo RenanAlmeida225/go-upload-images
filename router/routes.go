e436a289323dd4842ce2e64e5ff473c80781d514
italo renan <italorenan225@gmail.com>
2023-10-19 16:42:04 -0300

    feat: add handler of save images

--- router/routes.go
+++ router/routes.go
@@ -1,16 +1,11 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/RenanAlmeida225/go-upload-images/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
-	v1.POST("/images", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusCreated, gin.H{
-			"msg": "created",
-		})
-	})
+	v1.POST("/images", handler.SaveImageHandler)
 }
