9c60917b09f792162e3f1fd3345a975b80fdb4b0
italo renan <italorenan225@gmail.com>
2023-10-20 15:42:38 -0300

    feat: create initialize of postgres

---
+++ config/postgres.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/RenanAlmeida225/go-upload-images/schemas"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitializePostegres() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if err = db.AutoMigrate(&schemas.Images{}); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
