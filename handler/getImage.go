449a1b291a2f919abb6f10522a94a70ffe5eabea
italo renan <italorenan225@gmail.com>
2023-10-20 18:15:53 -0300

    feat: handler to get an image

---
+++ handler/getImage.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/RenanAlmeida225/go-upload-images/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func GetImage(ctx *gin.Context) {
+	id := ctx.Param("id")
+	image := schemas.Images{}
+	if err := db.First(&image, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "image not found")
+		return
+	}
+
+	send(ctx, http.StatusOK, "get an image", image)
+}
