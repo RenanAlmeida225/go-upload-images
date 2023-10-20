e3bcc211273b573760691a44e19d829a7ac74a6f
italo renan <italorenan225@gmail.com>
2023-10-20 15:04:18 -0300

    feat: implemation of save

--- helper/saveInS3.go
+++ helper/saveInS3.go
@@ -1,5 +1,34 @@
 package helper
 
-func SaveInS3() string {
-	return "Url"
+import (
+	"context"
+	"mime/multipart"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func SaveInS3(image *multipart.FileHeader, name string) (string, error) {
+	f, err := image.Open()
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	client := clientS3
+
+	uploader := manager.NewUploader(client)
+	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String("upload-images-go"),
+		Key:    aws.String(name),
+		Body:   f,
+		ACL:    "public-read",
+	})
+	if err != nil {
+
+		return "", err
+	}
+
+	return result.Location, nil
 }
