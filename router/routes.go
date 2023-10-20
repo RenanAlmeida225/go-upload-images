37a0ec8b5a29822e9de19170c490eaaa5123f9ea
italo renan <italorenan225@gmail.com>
2023-10-20 18:14:42 -0300

    feat: add new endpoints

--- router/routes.go
+++ router/routes.go
@@ -10,7 +10,10 @@ func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 	{
 		v1.POST("/images", handler.SaveImageHandler)
-		v1.DELETE("/images", handler.DeleteImage)
+		v1.GET("/images", handler.GetAllImages)
+		v1.GET("/images/:id", handler.GetImage)
+		v1.PATCH("/images/:id", handler.UpateImage)
+		v1.DELETE("/images/:id", handler.DeleteImage)
 	}
 
 }
