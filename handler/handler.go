fdb5b761614d1e7a619fa1aa894a6c340e31a5d1
italo renan <italorenan225@gmail.com>
2023-10-20 15:43:11 -0300

    feat: intatiate postgres

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,16 @@
 package handler
 
-import "github.com/RenanAlmeida225/go-upload-images/helper"
+import (
+	"github.com/RenanAlmeida225/go-upload-images/config"
+	"github.com/RenanAlmeida225/go-upload-images/helper"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
 
 func InitializeHandler() {
 	helper.InitializeHelper()
+	db = config.GetPostgres()
 }
