e373325d09d82625174f1647bd8af6a06dbe4190
italo renan <italorenan225@gmail.com>
2023-10-20 15:42:12 -0300

    feat: add initialization of postgres

--- config/config.go
+++ config/config.go
@@ -2,10 +2,12 @@ package config
 
 import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"gorm.io/gorm"
 )
 
 var (
 	clientS3 *s3.Client
+	db       *gorm.DB
 )
 
 func Init() error {
@@ -13,13 +15,24 @@ func Init() error {
 	if err != nil {
 		return err
 	}
+
 	clientS3, err = InitializeS3()
 	if err != nil {
 		return err
 	}
+
+	db, err = InitializePostegres()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func GetClientS3() *s3.Client {
 	return clientS3
+}
+
+func GetPostgres() *gorm.DB {
+	return db
 }
