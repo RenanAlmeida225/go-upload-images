02422817eea44d38cb8527bbaef593fa111cdc2a
italo renan <italorenan225@gmail.com>
2023-10-25 15:49:57 -0300

    feat: add middleware in routes

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/RenanAlmeida225/go-upload-images/internal/handler"
+	"github.com/RenanAlmeida225/go-upload-images/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +13,12 @@ func initializeRoutes(r *gin.Engine) {
 		v1.POST("/signUp", handler.SignUp)
 		v1.GET("/confirmation/:token", handler.ConfirmationEmail)
 		v1.POST("/signIn", handler.SignIn)
-		v1.POST("/images", handler.SaveImageHandler)
-		v1.GET("/images", handler.GetAllImages)
-		v1.GET("/images/:id", handler.GetImage)
-		v1.PATCH("/images/:id", handler.UpateImage)
-		v1.DELETE("/images/:id", handler.DeleteImage)
+
+		v1.POST("/images", middleware.VerifyAuth, handler.SaveImageHandler)
+		v1.GET("/images", middleware.VerifyAuth, handler.GetAllImages)
+		v1.GET("/images/:id", middleware.VerifyAuth, handler.GetImage)
+		v1.PATCH("/images/:id", middleware.VerifyAuth, handler.UpateImage)
+		v1.DELETE("/images/:id", middleware.VerifyAuth, handler.DeleteImage)
 	}
 
 }
