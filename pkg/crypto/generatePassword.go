9c672aa33b0a0bcc4418a024910ccb1df2845ffe
italo renan <italorenan225@gmail.com>
2023-10-25 13:23:32 -0300

    feat: generate password hashed

---
+++ pkg/crypto/generatePassword.go
@@ -0,0 +1,11 @@
+package crypto
+
+import "golang.org/x/crypto/bcrypt"
+
+func GeneratePassword(password string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
