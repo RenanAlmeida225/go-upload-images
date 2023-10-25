b51734ccf56f92f87ef921468eea87c10cfcc0cb
italo renan <italorenan225@gmail.com>
2023-10-25 07:56:14 -0300

    feat: add new routes to atuh

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -9,6 +9,9 @@ func initializeRoutes(r *gin.Engine) {
 	handler.InitializeHandler()
 	v1 := r.Group("/api/v1")
 	{
+		v1.POST("/signUp")
+		v1.GET("/confirmation/:token")
+		v1.POST("/signIn")
 		v1.POST("/images", handler.SaveImageHandler)
 		v1.GET("/images", handler.GetAllImages)
 		v1.GET("/images/:id", handler.GetImage)
