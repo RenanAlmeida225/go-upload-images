b2c701e32e808c2922b739617b193c70e3f4d35f
italo renan <italorenan225@gmail.com>
2023-10-26 07:50:15 -0300

    refactor: add verification to user_id

--- internal/handler/getImage.go
+++ internal/handler/getImage.go
@@ -8,9 +8,17 @@ import (
 )
 
 func GetImage(ctx *gin.Context) {
+	email, _ := ctx.Get("email")
+	user := schemas.User{}
+
+	if err := db.First(&user, "email=?", email).Error; err != nil {
+		sendError(ctx, http.StatusUnauthorized, "user not found")
+		return
+	}
+
 	id := ctx.Param("id")
 	image := schemas.Images{}
-	if err := db.First(&image, id).Error; err != nil {
+	if err := db.First(&image, "id=? and user_id=?", id, user.ID).Error; err != nil {
 		sendError(ctx, http.StatusNotFound, "image not found")
 		return
 	}
